76747f1de26033666a8fd2494434d37e408ddcc5
tangyuan <sipenghh@163.com>
2026-05-25 16:50:14 +0800

    feat: update debugger for python and debug ui

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -7,187 +7,163 @@
 -- kickstart.nvim and not kitchen-sink.nvim ;)
 return {
   {
+    "theHamsta/nvim-dap-virtual-text",
+    event = "VeryLazy",
+    opts = {
+      show_stop_reason = false,
+      virt_text_pos = 'eol',
+    },
+  },
+
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    config = function()
+      local function get_project_root()
+        local root = vim.fs.root(0, {
+          "uv.lock",
+          "pyproject.toml",
+          ".git",
+          "setup.py",
+          "setup.cfg",
+          "requirements.txt",
+          "environment.yml",
+          "conda.yaml",
+        })
+
+        return root or vim.fn.getcwd()
+      end
+
+      local function get_python()
+        local root = get_project_root()
+        local venv_python = root .. "/.venv/bin/python"
+
+        if vim.fn.executable(venv_python) == 1 then
+          return venv_python
+        end
+
+        local venv = os.getenv("VIRTUAL_ENV")
+        if venv and vim.fn.executable(venv .. "/bin/python") == 1 then
+          return venv .. "/bin/python"
+        end
+
+        local conda = os.getenv("CONDA_PREFIX")
+        if conda and vim.fn.executable(conda .. "/bin/python") == 1 then
+          return conda .. "/bin/python"
+        end
+
+        return "python3"
+      end
+
+      local function get_env()
+        return {
+          TOKENIZERS_PARALLELISM = "false",
+          OC_PYDEVD_RESOLVER = "DISABLE",
+        }
+      end
+
+      -- Adapter Python: 优先使用 Mason 安装的 debugpy
+      -- Target Python: 由每个 launch 配置里的 pythonPath 指定为项目 .venv
+      local mason_debugpy = vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python"
+
+      if vim.fn.executable(mason_debugpy) == 1 then
+        require("dap-python").setup(mason_debugpy)
+      else
+        require("dap-python").setup(get_python())
+      end
+
+      local dap = require("dap")
+
+      dap.configurations.python = {
+        {
+          type = "python",
+          request = "launch",
+          name = "file",
+          program = "${file}",
+          cwd = get_project_root,
+          pythonPath = get_python,
+          console = "integratedTerminal",
+          justMyCode = false,
+          env = get_env,
+        },
+
+        {
+          type = "python",
+          request = "launch",
+          name = "file:args",
+          program = "${file}",
+          cwd = get_project_root,
+          pythonPath = get_python,
+          console = "integratedTerminal",
+          justMyCode = false,
+          args = function()
+            local root = get_project_root()
+            vim.cmd("lcd " .. vim.fn.fnameescape(root))
+
+            local args = vim.fn.input({
+              prompt = "Arguments: ",
+              completion = "file",
+            })
+
+            return require("dap.utils").splitstr(args)
+          end,
+          env = get_env,
+        },
+
+        {
+          type = "python",
+          request = "launch",
+          name = "module:args",
+          module = function()
+            return vim.fn.input("Module: ")
+          end,
+          cwd = get_project_root,
+          pythonPath = get_python,
+          console = "integratedTerminal",
+          justMyCode = false,
+          args = function()
+            local root = get_project_root()
+            vim.cmd("lcd " .. vim.fn.fnameescape(root))
+
+            local args = vim.fn.input({
+              prompt = "Arguments: ",
+              completion = "file",
+            })
+
+            return require("dap.utils").splitstr(args)
+          end,
+          env = get_env,
+        },
+
+        {
+          type = "python",
+          request = "attach",
+          name = "attach",
+          connect = function()
+            local host = vim.fn.input("Host [127.0.0.1]: ")
+            host = host ~= "" and host or "127.0.0.1"
+
+            local port = tonumber(vim.fn.input("Port [5678]: ")) or 5678
+
+            return {
+              host = host,
+              port = port,
+            }
+          end,
+          cwd = get_project_root,
+          pythonPath = get_python,
+          justMyCode = false,
+        },
+      }
+    end,
+  },
+  {
     "mfussenegger/nvim-dap",
     dependencies = {
       "rcarriga/nvim-dap-ui",
       "jay-babu/mason-nvim-dap.nvim",
-
-      {
-        "theHamsta/nvim-dap-virtual-text",
-        opts = {
-          enabled = true,
-          enabled_commands = true,
-          highlight_changed_variables = false,
-          highlight_new_as_changed = false,
-          show_stop_reason = true,
-          commented = false,
-          only_first_definition = true,
-          all_references = false,
-          virt_text_pos = "eol",
-          all_frames = false,
-          virt_lines = false,
-        },
-      },
-
-      {
-        "mfussenegger/nvim-dap-python",
-        keys = {
-          {
-            "<leader>dPt",
-            function()
-              require("dap-python").test_method()
-            end,
-            desc = "Debug Method",
-            ft = "python",
-          },
-          {
-            "<leader>dPc",
-            function()
-              require("dap-python").test_class()
-            end,
-            desc = "Debug Class",
-            ft = "python",
-          },
-        },
-        config = function()
-          local function get_project_root()
-            local root = vim.fs.root(0, {
-              "uv.lock",
-              "pyproject.toml",
-              ".git",
-              "setup.py",
-              "setup.cfg",
-              "requirements.txt",
-              "environment.yml",
-              "conda.yaml",
-            })
-
-            return root or vim.fn.getcwd()
-          end
-
-          local function get_python()
-            local root = get_project_root()
-            local venv_python = root .. "/.venv/bin/python"
-
-            if vim.fn.executable(venv_python) == 1 then
-              return venv_python
-            end
-
-            local venv = os.getenv("VIRTUAL_ENV")
-            if venv and vim.fn.executable(venv .. "/bin/python") == 1 then
-              return venv .. "/bin/python"
-            end
-
-            local conda = os.getenv("CONDA_PREFIX")
-            if conda and vim.fn.executable(conda .. "/bin/python") == 1 then
-              return conda .. "/bin/python"
-            end
-
-            return "python3"
-          end
-
-          local function get_env()
-            return {
-              TOKENIZERS_PARALLELISM = "false",
-              OC_PYDEVD_RESOLVER = "DISABLE",
-            }
-          end
-
-          -- Adapter Python: 优先使用 Mason 安装的 debugpy
-          -- Target Python: 由每个 launch 配置里的 pythonPath 指定为项目 .venv
-          local mason_debugpy = vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python"
-
-          if vim.fn.executable(mason_debugpy) == 1 then
-            require("dap-python").setup(mason_debugpy)
-          else
-            require("dap-python").setup(get_python())
-          end
-
-          local dap = require("dap")
-
-          dap.configurations.python = {
-            {
-              type = "python",
-              request = "launch",
-              name = "file",
-              program = "${file}",
-              cwd = get_project_root,
-              pythonPath = get_python,
-              console = "integratedTerminal",
-              justMyCode = false,
-              env = get_env,
-            },
-
-            {
-              type = "python",
-              request = "launch",
-              name = "file:args",
-              program = "${file}",
-              cwd = get_project_root,
-              pythonPath = get_python,
-              console = "integratedTerminal",
-              justMyCode = false,
-              args = function()
-                local root = get_project_root()
-                vim.cmd("lcd " .. vim.fn.fnameescape(root))
-
-                local args = vim.fn.input({
-                  prompt = "Arguments: ",
-                  completion = "file",
-                })
-
-                return require("dap.utils").splitstr(args)
-              end,
-              env = get_env,
-            },
-
-            {
-              type = "python",
-              request = "launch",
-              name = "module:args",
-              module = function()
-                return vim.fn.input("Module: ")
-              end,
-              cwd = get_project_root,
-              pythonPath = get_python,
-              console = "integratedTerminal",
-              justMyCode = false,
-              args = function()
-                local root = get_project_root()
-                vim.cmd("lcd " .. vim.fn.fnameescape(root))
-
-                local args = vim.fn.input({
-                  prompt = "Arguments: ",
-                  completion = "file",
-                })
-
-                return require("dap.utils").splitstr(args)
-              end,
-              env = get_env,
-            },
-
-            {
-              type = "python",
-              request = "attach",
-              name = "attach",
-              connect = function()
-                local host = vim.fn.input("Host [127.0.0.1]: ")
-                host = host ~= "" and host or "127.0.0.1"
-
-                local port = tonumber(vim.fn.input("Port [5678]: ")) or 5678
-
-                return {
-                  host = host,
-                  port = port,
-                }
-              end,
-              cwd = get_project_root,
-              pythonPath = get_python,
-              justMyCode = false,
-            },
-          }
-        end,
-      },
+      "mfussenegger/nvim-dap-python",
+      "theHamsta/nvim-dap-virtual-text",
     },
     -- stylua: ignore
     keys = {
@@ -207,8 +183,6 @@ return {
       { "<leader>ds", function() require("dap").session() end, desc = "Session" },
       { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
       { "<leader>dh", function() require("dap.ui.widgets").hover() end, desc = "Hover Widget" },
-      { "<leader>dU", function() local dapui = require("dapui") dapui.close({}) vim.schedule(function() dapui.open({ reset = true }) end) end, desc = "Dap UI Reset Layout", },
-      { "<leader>dr", function() require("dapui").float_element("repl", { enter = true, width = 100, height = 25, position = "center", }) end, desc = "DAP REPL Float", },
 
       { "<F5>", function() require("dap").continue() end, desc = "Debug Start/Continue" },
       { "<F6>", function() require("dap").pause() end, desc = "Debug Pause" },
@@ -243,22 +217,12 @@ return {
   {
     "rcarriga/nvim-dap-ui",
     dependencies = { "nvim-neotest/nvim-nio" },
+    -- stylua: ignore
     keys = {
-      {
-        "<leader>du",
-        function()
-          require("dapui").toggle({})
-        end,
-        desc = "Dap UI",
-      },
-      {
-        "<leader>de",
-        function()
-          require("dapui").eval()
-        end,
-        desc = "Eval",
-        mode = { "n", "x" },
-      },
+      { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI", },
+      { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = { "n", "x" }, },
+      { "<leader>dr", function() require("dapui").close({}) vim.schedule(function() require("dapui").open({ reset = true }) end) end, desc = "Dap UI Reset Layout", },
+      { "<leader>do", function() require("dapui").float_element("console", { enter = true, width = 100, height = 25, position = "center", }) end, desc = "DAP Console Float", },
     },
     opts = {
       layouts = {
@@ -274,7 +238,7 @@ return {
         },
         {
           elements = {
-            { id = "console", size = 1.0 },
+            { id = "repl", size = 1.0 },
           },
           size = 8,
           position = "bottom",
@@ -283,7 +247,7 @@ return {
 
       controls = {
         enabled = true,
-        element = "console",
+        element = "repl",
         icons = {
           pause = " F6",
           play = " F5",
@@ -296,6 +260,7 @@ return {
           disconnect = " ",
         },
       },
+
       floating = {
         max_height = 0.75,
         max_width = 0.75,
@@ -306,8 +271,9 @@ return {
       },
 
       render = {
-        max_type_length = 80,
-        max_value_lines = 20,
+        max_type_length = 60,
+        max_value_lines = 6,
+        indent = 2,
       },
     },
     config = function(_, opts)
@@ -320,11 +286,10 @@ return {
         dapui.open({})
       end
 
-      -- 不自动关闭，避免大量输出、多线程、多进程时窗口闪烁
-      -- 手动用 <leader>du 或 F7 关闭
-      -- dap.listeners.before.event_terminated["dapui_config"] = function()
-      --   dapui.close({})
-      -- end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close({})
+      end
+
       dap.listeners.before.event_exited["dapui_config"] = function()
         dapui.close({})
       end
