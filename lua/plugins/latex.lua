8e6fb96fa0697105f2e69a3ad4b942f7f3e6583a
postpone <sipenghh@163.com>
2026-07-21 01:41:06 +0800

    fix: disable vimtex syntax conceal

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -3,6 +3,7 @@ return {
     "lervag/vimtex",
     lazy = false,
     init = function()
+      vim.g.vimtex_syntax_conceal_disable = 1
       vim.g.vimtex_compiler_method = "latexmk"
       vim.g.vimtex_view_method = "skim"
       vim.g.vimtex_view_skim_sync = 1
